11a53966e66b666952a6c0567b7591932251fff1
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2022-01-21 02:00:48 -0300

    Add files via upload

---
+++ Refactoring.rb
@@ -0,0 +1,28 @@
+#Before
+require 'prime'
+
+def first_n_primes(n)
+
+  unless n.is_a? Integer
+    return "n must be an integer."
+  end
+
+  if n <= 0
+    return "n must be greater than 0."
+  end
+
+  return Prime.first n
+end
+
+first_n_primes(10)
+
+#After
+require 'prime'
+
+def first_n_primes(n)
+  return "n must be an integer." unless n.is_a? Integer
+  return "n must be greater than 0." unless n > 0
+  Prime.first n
+end
+
+first_n_primes(10)
